d2898f8c1e96d8f92a73f1cbcf770af9ea12db60
Channing Hunter <channing.hunter@wustl.edu>
2013-10-20 20:17:21 -0500

    This is the newest version of everything so far

---
+++ MethodsList.java
@@ -0,0 +1,15 @@
+
+public class MethodsList {
+
+	public static void main(String[] args) {
+		int [][] Problem1 = new int [l][6];
+		int [][] Problem2 = new int [m][6];
+		
+	 public static boolean (int [l][0], int [l+1][0]);
+	 r
+	 
+	 	
+
+	}
+
+}
--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,8 +1,23 @@
+import java.io.DataInputStream;	
+import java.io.FileInputStream;
+
+
 
 public class SortingAlgorithm {
-
 	public static void main(String[] args) {
-		// import txt doc into the program 
-		//sort array based on the first value
+
+		DataInputStream reader = new DataInputStream(new FileInputStream("DistanceOutput.txt"));
+
+		int pProblems [][] = new int [l][6];
+		int NPProblems [][] = new int [m][6];
+		
+		if [1][0]==0
+
+
+
+
+
+
 	}
 }
+
